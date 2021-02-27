d01f249a05bdb273ecb401a9fc3adf0b2e769ac3
sshruti14 <shetty_14august@yahoo.com>
2021-02-27 15:32:18 -0500

    Adding code for deleting todos

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,36 @@
-import 'source-map-support/register'
+import "source-map-support/register";
+import * as middy from "middy";
+import { cors } from "middy/middlewares";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from "aws-lambda";
+import { deleteTodo } from "../../businessLogic/";
+import { createLogger } from "../../utils/logger";
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
-
+const deleteHandler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   // TODO: Remove a TODO item by id
-  return undefined
-}
+
+  const logger = createLogger("DeleteTodo");
+  logger.info("Event data:", event);
+
+  const todoId = event.pathParameters.todoId;
+  const authorization = event.headers.Authorization;
+  const split = authorization.split(" ");
+  const jwtToken = split[1];
+
+  await deleteTodo(todoId, jwtToken);
+  return {
+    statusCode: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: "Item deleted successfully",
+  };
+};
+
+export const handler = middy(deleteHandler).use(cors({ credentials: true }));
