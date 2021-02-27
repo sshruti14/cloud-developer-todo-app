b9b902da2a6273f33e7dafc4019118c8a3266fee
sshruti14 <shetty_14august@yahoo.com>
2021-02-27 15:24:36 -0500

    Adding code for fetching the todos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,39 @@
-import 'source-map-support/register'
+import "source-map-support/register";
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from "aws-lambda";
+import * as middy from "middy";
+import { cors } from "middy/middlewares";
+import { getAllTodos } from "../../businessLogic/";
+import { createLogger } from "../../utils/logger";
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const getHandler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
-}
+
+  const logger = createLogger("getAllTodos");
+  logger.info("Event data:", event);
+
+  const authHeader = event.headers.Authorization;
+  const authSplit = authHeader.split(" ");
+  const userId = authSplit[1];
+
+  const items = await getAllTodos(userId);
+  const result = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      items,
+    }),
+  };
+
+  return result;
+};
+
+export const handler = middy(getHandler).use(cors({ credentials: true }));
