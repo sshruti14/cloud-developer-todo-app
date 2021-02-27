81368a44b73a789af1647c5d1a3dcbaffc3cc402
sshruti14 <shetty_14august@yahoo.com>
2021-02-27 15:27:38 -0500

    Adding code for adding todo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,39 @@
-import 'source-map-support/register'
+import "source-map-support/register";
+import * as middy from "middy";
+import { cors } from "middy/middlewares";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from "aws-lambda";
+import { CreateTodoRequest } from "../../requests/CreateTodoRequest";
+import { createTodo } from "../../businessLogic/";
+import { createLogger } from "../../utils/logger";
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
-
+const createHandler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   // TODO: Implement creating a new TODO item
-  return undefined
-}
+
+  const logger = createLogger("CreateTodo");
+  logger.info("Event data:", event);
+
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
+  const authorization = event.headers.Authorization;
+  const split = authorization.split(" ");
+  const jwtToken = split[1];
+
+  const newItem = await createTodo(newTodo, jwtToken);
+  return {
+    statusCode: 201,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      newItem,
+    }),
+  };
+};
+
+export const handler = middy(createHandler).use(cors({ credentials: true }));
